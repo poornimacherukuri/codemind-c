7938ce031bf0a6f0d844aefee80660e72c4feffb
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2023-12-24 15:42:17 +0530

    #include<stdio.h> int main() {     int a,b,h,i;     scanf("%d%d",&a,&b);     for(i=1;i<a||i<b;i++)     {         if(a%i==0 && b%i==0)         {             h=i;         }     }     printf("%d",h); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,i,m,g=0;
-	scanf("%d%d",&a,&b);
-	for(i=1;i<=b;i++)
-	{
-		m=a*i;
-		if(m%b==0)
-		{
-			
-			break;
-		}
-	}
-
-	g=(a*b)/m;
-	printf("%d",g);
+    int a,b,h,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<a||i<b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            h=i;
+        }
+    }
+    printf("%d",h);
 }

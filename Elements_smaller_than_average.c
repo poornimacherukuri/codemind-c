f67115ba948aa2de11ae2f479bf2b954c1bc3057
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2023-12-31 14:25:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int a;     scanf("%d",&a);     int arr[a],sum=0;     for(int i=0;i<a;i++)     {         scanf("%d",&arr[i]);         sum=sum+arr[i];     }     int avg=sum/a,c=0;     for(int i=0;i<a;i++)     {         if(arr[i]<=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],sum=0;
+    for(int i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    int avg=sum/a,c=0;
+    for(int i=0;i<a;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}

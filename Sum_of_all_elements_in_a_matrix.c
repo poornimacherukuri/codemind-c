cd655ceee6466432ed56eb0e50669bf33ebc4d12
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2023-12-31 13:36:02 +0530

    #include<stdio.h> int main() {     int a[10][10],i,j,r,c,sum=0;     scanf("%d %d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             sum=sum+a[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],i,j,r,c,sum=0;
+    scanf("%d %d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            sum=sum+a[i][j];
+        }
+    }
+    printf("%d",sum);
+}
